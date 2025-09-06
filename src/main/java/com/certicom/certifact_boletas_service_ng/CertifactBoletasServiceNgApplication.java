bfa33e4c77fb56aa7cc2edfeaed463c745ee90f8
Jose Zambrano <21zam03.free@gmail.com>
2025-09-06 15:26:46 -0500

    new beginnning

---
+++ src/main/java/com/certicom/certifact_boletas_service_ng/CertifactBoletasServiceNgApplication.java
@@ -0,0 +1,13 @@
+package com.certicom.certifact_boletas_service_ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifactBoletasServiceNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifactBoletasServiceNgApplication.class, args);
+	}
+
+}
