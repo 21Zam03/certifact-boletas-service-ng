fff521d8aa42408d7cef634ea36d6ce182592e09
Jose Zambrano <21zam03.free@gmail.com>
2025-09-16 15:04:24 -0500

    update paymentefeign

--- src/main/java/com/certicom/certifact_boletas_service_ng/feign/PaymentVoucherFeign.java
+++ src/main/java/com/certicom/certifact_boletas_service_ng/feign/PaymentVoucherFeign.java
@@ -2,10 +2,10 @@ package com.certicom.certifact_boletas_service_ng.feign;
 
 import com.certicom.certifact_boletas_service_ng.dto.PaymentVoucherDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @FeignClient(name = "boletas-service-sp", url = "http://localhost:8092", contextId = "paymentVoucher")
 public interface PaymentVoucherFeign {
